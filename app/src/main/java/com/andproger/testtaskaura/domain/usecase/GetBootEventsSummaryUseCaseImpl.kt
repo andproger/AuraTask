f353cdc98ee44da45b47de3b5f1f2c05342b1032
andproger <andryproger@gmail.com>
2024-07-02 10:07:02 +0200

    fixed date mapping for boot events summary

--- app/src/main/java/com/andproger/testtaskaura/domain/usecase/GetBootEventsSummaryUseCaseImpl.kt
+++ app/src/main/java/com/andproger/testtaskaura/domain/usecase/GetBootEventsSummaryUseCaseImpl.kt
@@ -3,26 +3,36 @@ package com.andproger.testtaskaura.domain.usecase
 import com.andproger.testtaskaura.domain.model.BootEventSummary
 import com.andproger.testtaskaura.domain.repository.BootEventRepository
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.map
+import java.util.Calendar
 import java.util.Date
-import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 
 class GetBootEventsSummaryUseCaseImpl @Inject constructor(
     private val bootEventRepository: BootEventRepository
 ) : GetBootEventsSummaryUseCase {
+
     override fun getSummary(): Flow<BootEventSummary> {
         return bootEventRepository.getBootEventsFlow().map { events ->
             val eventsPerDay = events.groupBy { event ->
-                val millisInDay = TimeUnit.DAYS.toMillis(1)
-                Date(event.timestamp / millisInDay * millisInDay)
+                toStartOfDay(Date(event.timestamp))
             }.mapValues { entry ->
                 entry.value.size
             }
 
             BootEventSummary(eventsPerDay)
         }
+    }
+
+    //TODO could be moved to some util class
+    private fun toStartOfDay(date: Date): Date {
+        val calendar = Calendar.getInstance()
+        calendar.time = date
+        calendar.set(Calendar.HOUR_OF_DAY, 0)
+        calendar.set(Calendar.MINUTE, 0)
+        calendar.set(Calendar.SECOND, 0)
+        calendar.set(Calendar.MILLISECOND, 0)
+        return calendar.time
     }
 }
